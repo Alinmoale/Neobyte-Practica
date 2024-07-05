966a53bc718d1932dba2bd3cc77490442f60e919
Alin Moale <moalealin15@gmail.com>
2024-07-05 14:04:08 +0300

    db_config

--- Project3/index.php
+++ Project3/index.php
@@ -1,18 +1,12 @@
 <?php
-$host = "127.0.0.1"; 
-$username = "root";
-$password = "pass";
-$dbname = "Movies";
-
-$default_limit = 10; // default limit
-$default_offset = 0; // default offset
+require_once 'db_config.php';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 // Retrieve data from the database
-$offset = isset($_GET['offset'])? (int) $_GET['offset'] : $default_offset;
-$limit = isset($_GET['limit'])? (int) $_GET['limit'] : $default_limit;
+$offset = isset($_GET['offset'])? (int) $_GET['offset'] : 0;
+$limit = isset($_GET['limit'])? (int) $_GET['limit'] : 10;
 
 $stmt = $conn->prepare("SELECT m.id, m.title, c.name AS category FROM movies m JOIN categories c ON m.category_id = c.id ORDER BY m.id LIMIT :limit OFFSET :offset");
 $stmt->bindParam(':limit', $limit, PDO::PARAM_INT);
